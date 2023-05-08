88b97a3b98108a9da27ae657ffa15407d9a80edb
Kang JiHoon <shb04016@gmail.com>
2023-05-08 23:15:31 +0900

    initial commit

---
+++ src/main/java/jscode/jscodeproject/JscodeProjectApplication.java
@@ -0,0 +1,13 @@
+package jscode.jscodeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JscodeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JscodeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jscode/jscodeproject/JscodeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jscode.jscodeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JscodeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
