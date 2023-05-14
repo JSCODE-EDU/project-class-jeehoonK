b021516847f4db12694a93433699ff737aadb577
Kang JiHoon <shb04016@gmail.com>
2023-05-14 23:11:39 +0900

    feat. 게시글 도메인 및 레퍼지토리, 서비스 구현

---
+++ src/main/java/jscode/jscodeproject/service/BulletinService.java
@@ -0,0 +1,42 @@
+package jscode.jscodeproject.service;
+
+
+import jscode.jscodeproject.domain.Bulletin;
+import jscode.jscodeproject.repository.BulletinRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class BulletinService {
+
+    private final BulletinRepository bulletinRepository;
+
+    //글 작성
+    @Transactional
+    public Long write(Bulletin bulletin){
+        bulletinRepository.save(bulletin);
+        return bulletin.getId();
+    }
+
+    //전체 게시물 찾기
+    public List<Bulletin> findBulletins() {
+        return bulletinRepository.findAll();
+    }
+
+    //Id로 하나찾기
+    public Bulletin findOne(Long bulletinId){
+        return bulletinRepository.findById(bulletinId);
+    }
+
+    //삭제
+    @Transactional
+    public void deleteOne(Long bulletinId){
+        bulletinRepository.deleteById(bulletinId);
+    }
+
+}
