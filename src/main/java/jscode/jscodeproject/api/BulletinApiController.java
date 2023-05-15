5787cb2e1291b51776a020c8766ed0fe80740704
Kang JiHoon <shb04016@gmail.com>
2023-05-15 22:07:36 +0900

    refactor. 상태코드 추가 및 컨트롤러 리팩토링

--- src/main/java/jscode/jscodeproject/api/BulletinApiController.java
+++ src/main/java/jscode/jscodeproject/api/BulletinApiController.java
@@ -3,46 +3,51 @@ package jscode.jscodeproject.api;
 import jscode.jscodeproject.domain.Bulletin;
 import jscode.jscodeproject.service.BulletinService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController //controller + responsebody
 @RequiredArgsConstructor
+@RequestMapping("/bulletins")
 public class BulletinApiController {
 
     private final BulletinService bulletinService;
 
-    @PostMapping("/bulletins")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
     public Bulletin saveBulletin(@RequestBody Bulletin bul){
-        Bulletin bulletin = new Bulletin();
-        bulletin.setTitle(bul.getTitle());
-        bulletin.setText(bul.getText());
-        bulletinService.write(bulletin);
-        return bulletin;
+        bulletinService.write(bul);
+        return bul;
     }
 
-    @GetMapping("/bulletins")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
     public List<Bulletin> searchAll() {
         List<Bulletin> bulletins = bulletinService.findBulletins();
         return bulletins;
     }
 
-    @GetMapping("/bulletins/{bulletinId}")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{bulletinId}")
     public Bulletin searchById(@PathVariable Long bulletinId){
         Bulletin bulletin = bulletinService.findOne(bulletinId);
         return bulletin;
     }
 
-    @PatchMapping("/bulletins/{bulletinId}")
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/{bulletinId}")
     public Bulletin updateBulletin(@PathVariable Long bulletinId, @RequestBody Bulletin bul){
         Bulletin bulletin = bulletinService.updateBulletin(bulletinId, bul.getTitle(), bul.getText());
         return bulletin;
     }
 
-    @DeleteMapping("/bulletins/{bulletinId}")
-    public void deleteBulletin(@PathVariable Long bulletinId){
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/{bulletinId}")
+    public String deleteBulletin(@PathVariable Long bulletinId){
         bulletinService.deleteOne(bulletinId);
+        return "ok";
     }
 
 }
--- src/main/java/jscode/jscodeproject/domain/Bulletin.java
+++ src/main/java/jscode/jscodeproject/domain/Bulletin.java
@@ -4,21 +4,19 @@ package jscode.jscodeproject.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
 public class Bulletin {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "bulletin_id")
     private Long id;
 
     private String title;
     private String text;
+
 
     public void update(String title, String text) {
         this.title = title;
